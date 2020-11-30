6749072c791e5eb8c75254fe356cde748595ec03
zhoulin1600 <45185908@qq.com>
2020-11-30 19:45:38 +0800

    Publish

--- Hugo.Publish.Linux.sh
+++ Hugo.Publish.Linux.sh
@@ -1,6 +1,6 @@
 git pull;
 rm -rf Publish;
 dotnet build;
-dotnet publish -o /home/Hugo/Hugo.Core.WebApi/bin/Debug/netcoreapp3.1;
-cp -r /home/Hugo/Hugo.Core.Api/bin/Debug/netcoreapp3.1 Publish;
+dotnet publish -o /home/Hugo.Core.Api/Hugo.Core.WebApi/bin/Debug/netcoreapp3.1;
+cp -r /home/Hugo.Core.Api/Hugo.Core.WebApi/bin/Debug/netcoreapp3.1 Publish;
 echo "Successfully! Please see the file Publish";
